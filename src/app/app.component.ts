0143909f3ebbffa4b14ae8f1f81a220a1568a801
Faber Ordoñez <faberordonez1@gmail.com>
2022-02-06 16:53:03 -0500

    Commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { switchMap, interval, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'rxjs';
+
+  constructor() {}
+
+  public ngOnInit():void {
+    /**
+     * fromEvent => Obs detecta click
+     * SwitchMap => Interrumpe el flujo del obs (interval) con cada click
+     * interval(1000) => contador que incrementa en 1 cada segundo 
+     */
+    
+    fromEvent(document, 'click')
+      .pipe(switchMap(() => interval(1000)))
+      .subscribe(console.log);
+  }
+
 }
